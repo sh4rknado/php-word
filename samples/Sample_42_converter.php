39328629507358356ee4784900fed4c7de5ba756
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2020-05-02 21:47:55 +0200

    Modify Sample

--- samples/Sample_42_converter.php
+++ samples/Sample_42_converter.php
@@ -2,16 +2,16 @@
 
 require_once "../vendor/autoload.php";
 
+
 use PhpOffice\PhpWord\Converter\Converter;
 
-$converter = new Converter();
 
+$converter = new Converter();
 
 /**
  * Convert to HTML
  */
 $converter->docxToHtml( "resources/result.docx","results/result.html");
-
 
 
 /**
